2bbefae55d4a60f860ac2c282952f16bbbdc96ed
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-03-10 22:02:33 +0100

    Added specifications

--- opt/telemetry/webapp.py
+++ opt/telemetry/webapp.py
@@ -4,8 +4,10 @@ from threading import Thread, Event
 from colorama import Style
 
 class Webapp(Thread):
+    """ Thread for the management of the webapp. """
 
     def __init__(self, provider):
+        """ Initialize web framework, set provider and routing.  """
         Thread.__init__(self, name="webapp_thread", daemon=True)
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = 'f7acbeb708f93e5e81e8a55238aae4bbc75f69ac36d18d0c'
@@ -17,14 +19,17 @@ class Webapp(Thread):
         self.end_setup = Event()
 
     def index(self):
+        """ Index page handler. Simply render the 'index.html' template in the 'templates' folder. """
         return render_template('index.html', speed=self.provider.speed.actual, heading=self.provider.heading.actual)
 
     def update(self):
+        """ Background task for updating live data. Emit a websocket event with updates. """
         while True:
             self.socketio.sleep(0.5)
             self.socketio.emit('update', {'speed': self.provider.speed.actual, 'heading': self.provider.heading.actual})
 
     def run(self):
+        """ Start update task and enter WSGI server (eventlet) main loop. """
         self.socketio.start_background_task(self.update)
 
         print(f"{Style.DIM}[{self.getName()}] Setup finished{Style.RESET_ALL}")
