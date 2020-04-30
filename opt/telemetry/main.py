6cec726ebf470f51468aa5104f66c72180f76b13
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-04-30 19:34:28 +0200

    ~ moved chromium settings out from python code

--- opt/telemetry/main.py
+++ opt/telemetry/main.py
@@ -2,12 +2,7 @@ from sensors_provider import SensorsProvider
 from webapp import Webapp
 from colorama import Fore, Style
 from logger import Logger
-import subprocess, json
 
-def terminate(return_code):
-    logger.stop_log()
-    chrome.terminate()
-    exit(return_code)
 
 # Start the data provider thread (actually is only the gpsd thread)
 provider = SensorsProvider()
@@ -30,25 +25,10 @@ if not provider.end_setup.isSet() or \
    not webapp.end_setup.isSet() or \
    not logger.end_setup.isSet():
     print(f"{Fore.RED}[main_thread] Something went wrong during threads initialization, quitting...{Fore.RESET}")
-    terminate(1)
+    exit(1)
 
 # Wait all threads to start
 print(f"{Style.BRIGHT}[main_thread] Telemetry system started (CTRL+C to stop){Style.RESET_ALL}")
-
-# Edit chromium preferences
-preferences_file = open('/home/pi/.config/chromium/Default/Preferences', 'r')
-preferences = json.load(preferences_file)
-preferences_file.close()
-preferences['exit_type'] = 'Normal'
-preferences['exited_cleanly'] = True
-preferences_file = open('/home/pi/.config/chromium/Default/Preferences', 'w')
-json.dump(preferences, preferences_file)
-preferences_file.close()
-
-# Start chromium browser in kiosk mode
-chrome = subprocess.Popen(['chromium-browser', '--noerrdialogs', '--disable-infobars', '--kiosk', 'localhost/view'],
-                          stdout=subprocess.DEVNULL,
-                          stderr=subprocess.DEVNULL)
 
 # Watch threads and try recovery when needed, terminate program when KeyboardInterrupt is caught
 while True:
@@ -63,7 +43,7 @@ while True:
                 print(f"{Fore.GREEN}[main_thread] Done recovery!{Fore.RESET}")
             else:
                 print(f"{Fore.RED}[main_thread] Recovery failed, quitting...{Fore.RESET}")
-                terminate(1)
+                exit(1)
     except KeyboardInterrupt:
         print(f"{Fore.RED}[main_thread] KeyboardInterrupt caught, quitting...{Fore.RESET}")
-        terminate(0)
+        exit(0)
