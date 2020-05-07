b0117dc89b6b82374bc67668520c02e562eebe19
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-05-07 15:34:55 +0200

    Sensor generalization
    + Added general class Sensor
    + Defined common structure: DataTree with TimeoutVar
    + Added doctrings
    + Defined common access strategy: paths
    - Deprecated Webapp class

--- opt/telemetry/main.py
+++ opt/telemetry/main.py
@@ -1,49 +1,29 @@
-from sensors_provider import SensorsProvider
-from webapp import Webapp
+from provider import SensorProvider
 from colorama import Fore, Style
 from logger import Logger
 
 
-# Start the data provider thread (actually is only the gpsd thread)
-provider = SensorsProvider()
+# Start the sensors provider thread
+provider = SensorProvider()
 provider.start()
 
 # Start the logger thread
 logger = Logger(provider)
 logger.start()
 
-# Start the web thread
-webapp = Webapp(provider, logger)
-webapp.start()
-
 # Wait until all the threads have finished initializing (or exit after timeout)
 provider.end_setup.wait(timeout=20)
-webapp.end_setup.wait(timeout=20)
 logger.end_setup.wait(timeout=20)
 
-if not provider.end_setup.isSet() or \
-   not webapp.end_setup.isSet() or \
-   not logger.end_setup.isSet():
+if not provider.end_setup.isSet() or not logger.end_setup.isSet():
     print(f"{Fore.RED}[main_thread] Something went wrong during threads initialization, quitting...{Fore.RESET}")
     exit(1)
 
 # Wait all threads to start
 print(f"{Style.BRIGHT}[main_thread] Telemetry system started (CTRL+C to stop){Style.RESET_ALL}")
 
-# Watch threads and try recovery when needed, terminate program when KeyboardInterrupt is caught
 while True:
-    try:
-        if not provider.is_alive():
-            print(f"{Fore.YELLOW}[main_thread] Provider dead, attempting recovery...{Fore.RESET}")
-            provider = SensorsProvider()
-            provider.start()
-            provider.end_setup.wait(timeout=20)
-            if provider.end_setup.isSet():
-                webapp.provider = provider.get_sensor('gps')
-                print(f"{Fore.GREEN}[main_thread] Done recovery!{Fore.RESET}")
-            else:
-                print(f"{Fore.RED}[main_thread] Recovery failed, quitting...{Fore.RESET}")
-                exit(1)
+    try: pass
     except KeyboardInterrupt:
         print(f"{Fore.RED}[main_thread] KeyboardInterrupt caught, quitting...{Fore.RESET}")
         exit(0)
---
+++ opt/telemetry/provider.py
@@ -0,0 +1,60 @@
+from threading import Thread, Event
+from colorama import Style, Fore
+from mqtt import MqttSensor
+from gpsd import GpsdSensor
+
+SENSORS_LUT = {
+    'accel': ['#'],
+    'gps': ['TPV/mode', 'TPV/speed', 'TPV/track'],
+    'wind': ['#']
+}
+
+
+class SensorProvider(Thread):
+    """
+    Class for the gathering of data coming through sensors. Each sensor can be accessed by classical python array
+    access with square brackets. Sensor data can be accessed with paths that has the sensor name as the first key. \n
+    Example: sensor_provider['sensor_name/path/to/variable']
+    """
+
+    def __init__(self):
+        """ Initialize the sensor provider. """
+        Thread.__init__(self, name='sensors_provider', daemon=True)
+        self.__sensors = None
+        self.end_setup = Event()
+
+    def __getitem__(self, path: str):
+        """
+        Access sensor data by the given path.
+        :param path: The path as a slash separated keys string.
+        :return: The item correspondent to the path.
+        """
+        path = path.strip('/')
+        if not path: return {name: sensor['/'] for name, sensor in self.__sensors.items()}
+        if not path.split('/')[1:]: return self.__sensors[path.split('/')[0]]['/']
+        return self.__sensors[path.split('/')[0]][path[path.find('/'):]]
+
+    def __iter__(self):
+        """
+        Return an iterator through each sensor of the provider.
+        :return: The iterator object.
+        """
+        return self.__sensors.items().__iter__()
+
+    def run(self):
+        """ Main routine of the thread. Initialize and start sensors. """
+        self.__sensors = {
+            'accel': MqttSensor('accel', [f'sensor/accel/{topic}' for topic in SENSORS_LUT['accel']]),
+            'gps': GpsdSensor('gps', SENSORS_LUT['gps']),
+            'wind': MqttSensor('wind', [f'sensor/wind/{topic}' for topic in SENSORS_LUT['wind']])
+        }
+        for sensor in self.__sensors.values(): sensor.start()
+        for sensor in self.__sensors.values(): sensor.end_setup.wait(timeout=20)
+
+        if False in [sensor.end_setup.isSet() for sensor in self.__sensors.values()]:
+            print(f"{Fore.RED}[{self.getName()}] Sensors initialization failed, quitting...")
+            return
+        print(f"{Style.DIM}[{self.getName()}] Setup finished{Style.RESET_ALL}")
+        self.end_setup.set()
+
+        while True: pass
