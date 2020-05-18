c1ac9b495e0eac412c988193730e69ef94e5b083
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-05-19 01:44:15 +0200

    + Improved packet tracking

--- opt/telemetry/net/lora_transceiver.py
+++ opt/telemetry/net/lora_transceiver.py
@@ -8,6 +8,8 @@ from serial.threaded import LineReader, ReaderThread
 
 NULL_DATA = '-'
 
+# TODO: Add synchronization mechanism
+
 
 class LoraTransceiver(Thread):
     """ Class communicates via UART interface with an attached LoRa transceiver. """
@@ -51,44 +53,67 @@ class LoraTransceiver(Thread):
             lon = self.provider['gps/TPV/lon']
             speed = self.provider['gps/TPV/speed']
             track = self.provider['gps/TPV/track']
-            line = LineBuilder() \
+            line = LoraMessage() \
                 .append(str(lat) if lat is not None else NULL_DATA) \
                 .append(str(lon) if lon is not None else NULL_DATA) \
                 .append(str(round(speed, 1)) if speed is not None else NULL_DATA) \
                 .append(str(round(track)) if track is not None else NULL_DATA)
             self.protocol.write_line(line.build())
+            print(self.protocol)
 
 
 class LoraProtocol(LineReader):
-    def __init__(self):
+    def __init__(self, ack_rate=5, lost_timeout=2):
         super().__init__()
+        self.sent_packets = 0
         self.untracked_packets = 0
-        self.ack_rate = 5
+        self.received_packets = 0
+        self.lost_packets = 0
+        self.ack_rate = ack_rate
+        self.lost_timeout = lost_timeout
 
     def connection_made(self, transport):
         super().connection_made(transport)
 
     def write_line(self, text):
         super().write_line(text)
-        self.untracked_packets += 1
-        print(f"Untracked packets: {self.untracked_packets}")
+        self.sent_packets += 1
+        if self.untracked_packets >= self.lost_timeout * self.ack_rate:
+            self.lost_packets += 1
+        else: self.untracked_packets += 1
 
     def handle_line(self, line):
-        if line == 'ACK': self.untracked_packets -= self.ack_rate
+        line = LoraMessage.parse(line)
+        if line == 'ACK':
+            self.received_packets += self.ack_rate
+            if self.untracked_packets > self.ack_rate:
+                self.lost_packets += self.untracked_packets - self.ack_rate
+            self.untracked_packets = 0
 
     def connection_lost(self, exc):
         pass
 
+    def __str__(self):
+        return f"Sent packets: {self.sent_packets}\n" \
+               f"Untracked packets: {self.untracked_packets}\n" \
+               f"Received packets: {self.received_packets}\n" \
+               f"Lost packets: {self.lost_packets}\n"
+    __repr__ = __str__
 
-class LineBuilder:
-    def __init__(self, start='', separator=' '):
-        self.line = start
+
+class LoraMessage:
+    def __init__(self, header='', separator=' '):
+        self.message = header
         self.separator = separator
 
-    def append(self, segment):
-        if self.line: self.line += self.separator
-        self.line += segment
+    def append(self, field):
+        if self.message: self.message += self.separator
+        self.message += field
         return self
 
-    def build(self):
-        return self.line
+    def build(self, footer=''):
+        return self.message + footer
+
+    @staticmethod
+    def parse(message, separator=' '):
+        return message.split(separator) if separator in message else message
