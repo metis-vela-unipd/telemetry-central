ccad051c31d80d563eda17a4db085c4ae2c496ff
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-07-23 20:56:58 +0200

    Cleaned project

--- opt/telemetry/net/__init__.py
+++
@@ -1,7 +0,0 @@
-from providers import Provider
-from .lora import *
-from .websocket import *
-
-
-def get_gateway(source, name=None, tx_rate=5, filters=None):
-    if isinstance(source, Provider): return LoraTransceiver(source, name, tx_rate, filters)
