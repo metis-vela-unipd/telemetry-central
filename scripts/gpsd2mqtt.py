34870b01880052bbbdc3ee05d7cbf5c056ff2136
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-07-28 14:41:39 +0200

    Change var name

--- scripts/gpsd2mqtt.py
+++ scripts/gpsd2mqtt.py
@@ -2,8 +2,8 @@ from gps import gps, WATCH_ENABLE, WATCH_NEWSTYLE, MPS_TO_KNOTS
 from paho.mqtt.client import Client
 
 session = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
-mqtt = Client('gps0')
-mqtt.connect('localhost')
+client = Client('gps0')
+client.connect('localhost')
 
 for report in session:
     report = dict(report)
@@ -15,5 +15,5 @@ for report in session:
             report['speed'] *= MPS_TO_KNOTS
         for key, val in report.items():
             topic = f'sensor/gps0/{key}'
-            mqtt.publish(topic, str(val))
+            client.publish(topic, str(val))
             print(f'Publish! Topic: {topic}; Value: {val}')
