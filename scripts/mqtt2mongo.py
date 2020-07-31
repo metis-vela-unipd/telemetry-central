07399d106c19fd272e51fcc29b48454f0920d9c1
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-07-31 12:27:40 +0200

    Add tag system

--- scripts/mqtt2mongo.py
+++ scripts/mqtt2mongo.py
@@ -4,22 +4,29 @@ from paho.mqtt.client import Client
 from pymongo import MongoClient
 
 
-def on_message(client, userdata, message):
-    now = datetime.now()
-    document = {
-        'topic': message.topic,
-        'payload': message.payload.decode(),
-        'qos': message.qos,
-        'timestamp': int(now.timestamp()),
-        'datetime': now.strftime('%d/%m/%Y %H:%M:%S')
-    }
-    data.insert_one(document)
-    print(f"Store! Date: {now.strftime('%d/%m/%Y %H:%M:%S')}; Topic: {message.topic}; Payload: {message.payload.decode()}")
+class Store:
+    def __init__(self):
+        self.tag = ''
+
+    def on_message(self, client, userdata, message):
+        if message.topic == 'data/tag':
+            self.tag = message.payload.decode()
+        now = datetime.now()
+        document = {
+            'topic': message.topic,
+            'payload': message.payload.decode(),
+            'tag': self.tag,
+            'qos': message.qos,
+            'timestamp': int(now.timestamp()),
+            'datetime': now.strftime('%d/%m/%Y %H:%M:%S')
+        }
+        data.insert_one(document)
+        print(f"Store! Date: {now.strftime('%d/%m/%Y %H:%M:%S')}; Topic: {message.topic}; Payload: {message.payload.decode()}; Tag: '{self.tag}'")
 
 
 mongo = MongoClient()
 mqtt = Client('mongodb')
-mqtt.on_message = on_message
+mqtt.on_message = Store().on_message
 mqtt.connect('localhost')
 mqtt.subscribe('#')
 db = mongo['telemetry']
