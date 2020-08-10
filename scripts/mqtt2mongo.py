1352410c51628f759c92c3ea2708b4ebf075a2ea
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-08-10 18:02:30 +0200

    Add global tag

--- scripts/mqtt2mongo.py
+++ scripts/mqtt2mongo.py
@@ -2,33 +2,35 @@ from datetime import datetime
 
 from paho.mqtt.client import Client
 from pymongo import MongoClient
+from time import sleep
 
 
-class Store:
-    def __init__(self):
-        self.tag = ''
-
-    def on_message(self, client, userdata, message):
-        if message.topic == 'data/tag':
-            self.tag = message.payload.decode()
-        now = datetime.now()
-        document = {
-            'topic': message.topic,
-            'payload': message.payload.decode(),
-            'tag': self.tag,
-            'qos': message.qos,
-            'timestamp': int(now.timestamp()),
-            'datetime': now.strftime('%d/%m/%Y %H:%M:%S')
-        }
-        data.insert_one(document)
-        print(f"Store! Date: {now.strftime('%d/%m/%Y %H:%M:%S')}; Topic: {message.topic}; Payload: {message.payload.decode()}; Tag: '{self.tag}'")
+def on_message(client, userdata, message):
+    if message.topic == 'control/log/tag':
+        global tag
+        tag = message.payload.decode()
+        return
+    if message.topic == 'control/log/rate':
+        global rate
+        rate = float(message.payload.decode())
+        return
+    data[message.topic] = message.payload
 
 
 mongo = MongoClient()
 mqtt = Client('mongodb')
-mqtt.on_message = Store().on_message
+mqtt.on_message = on_message
 mqtt.connect('localhost')
 mqtt.subscribe('#')
 db = mongo['telemetry']
-data = db['data']
-mqtt.loop_forever()
+data = {}
+tag = ''
+rate = 2
+mqtt.loop_start()
+while True:
+    now = datetime.now()
+    data['timestamp'] = int(now.timestamp())
+    data['datetime'] = now.strftime('%d-%m-%Y %H:%M:%S')
+    mongo.telemetry.data.insert_one(data)
+    data = {}
+    sleep(1/rate)
